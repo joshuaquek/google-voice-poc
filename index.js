9251145ceee8c476c76d0e94e7398d6a2f789701
joshua.quek <joshua.quek@accenture.com>
2018-06-22 23:46:33 +0800

    Removed time ended issue

--- index.js
+++ index.js
@@ -1,50 +1,39 @@
 function streamingMicRecognize(encoding, sampleRateHertz, languageCode) {
-  // [START speech_streaming_mic_recognize]
+
+  // Import Modules
   const record = require('node-record-lpcm16');
   const { exec } = require('child_process');
-
-  // Imports the Google Cloud client library
   const speech = require('@google-cloud/speech');
 
   // Creates a client
   const client = new speech.SpeechClient();
-
-  /**
-   * TODO(developer): Uncomment the following lines before running the sample.
-   */
-  // const encoding = 'Encoding of the audio file, e.g. LINEAR16';
-  // const sampleRateHertz = 16000;
-  // const languageCode = 'BCP-47 language code, e.g. en-US';
-
   const request = {
     config: {
       encoding: encoding,
       sampleRateHertz: sampleRateHertz,
       languageCode: languageCode,
     },
-    interimResults: true // If you want interim results, set this to true
+    interimResults: false // If you want interim results, set this to true
   };
 
   // Create a recognize stream
   const recognizeStream = client
     .streamingRecognize(request)
     .on('error', console.error)
-    .on('data', (data) =>{
-      
-          exec('clear', function callback(error, stdout, stderr) {
-            console.log(stdout)
-            process.stdout.write(
-            data.results[0] && data.results[0].alternatives[0] ?
-              `${JSON.stringify(data.results[0].alternatives[0])}\n` :
-              `\n\nReached transcription time limit, press Ctrl+C\n`)
-          });
-        
-      }
-    );
+    .on('data', (data) => {
+      exec('clear', function callback(error, stdout, stderr) {
+        if (data.results[0] && data.results[0].alternatives[0]) {
+          process.stdout.write(`${JSON.stringify(data.results[0].alternatives[0])}\n`)
+          recognizeStream.end()
+          streamingMicRecognize('LINEAR16', 16000, 'en-GB')
+        } else {
+          process.stdout.write(`\n\nReached transcription time limit, press Ctrl+C\n`)
+        }
+      });
+    });
 
   // Start recording and send the microphone input to the Speech API
-  record
-    .start({
+  record.start({
       sampleRateHertz: sampleRateHertz,
       threshold: 0,
       // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
@@ -54,10 +43,9 @@ function streamingMicRecognize(encoding, sampleRateHertz, languageCode) {
     })
     .on('error', console.error)
     .pipe(recognizeStream);
-
-  console.log('Listening, press Ctrl+C to stop.');
-  // [END speech_streaming_mic_recognize]
+  // [END speech_streaming_mic_recognize
 }
 
 // Call the function
 streamingMicRecognize('LINEAR16', 16000, 'en-GB')
+
